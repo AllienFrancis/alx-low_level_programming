a54b0f3b351b5a46507ee26f65c8e05e35b9ca76
AllienFrancis <jigsonjo@yahoo.com>
2022-10-18 15:17:16 -0700

    Sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - calculating the absolute value ofo the number from zero
+ * @i: the integer to be converted
+Return: absolute value oof the input number.
+*/
+
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
