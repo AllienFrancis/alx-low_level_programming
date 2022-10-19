bc9c929d973229dadc975df64b5d7a3a8ce569b3
AllienFrancis <jigsonjo@yahoo.com>
2022-10-19 04:17:47 -0700

    Longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,27 @@
 
 void jack_bauer(void)
 {
-	int h, m;
+	int h1, h2, m1, m2;
 
-	for (h = 0; h < 24; h++)
+	for (h1 = 0; h1 < 3; h1++)
 	{
-		for (m = 0; m < 60; m++)
+		for (h2 = 0; h2 < 10; h2++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putcaher(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
+			if ((h1 == 2) && (h2 >= 4))
+				;
+			else
+			{
+				for (m1 = 0; m1 < 6; m1++)
+				{
+					_putchar(h1 + 48);
+					_putchar(h2 + 48);
+					_putchar(58);
+					_putchar(m1 + 48);
+					_putchar(m2 + 48);
+					_putchar(10);
+
+				}
+			}
 		}
 	}
 }
