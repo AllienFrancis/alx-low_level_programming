925769cc3b693b5a266c70710b3a094d931e3093
AllienFrancis <jigsonjo@yahoo.com>
2022-10-20 16:49:21 -0700

    Get the positive and negative function to work

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negtive(i);
+
+	printf("%d\n");
+
+	return (0);
+
+}
