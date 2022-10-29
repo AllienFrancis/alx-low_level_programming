19b3ca8c438054d0e548a6b784c562f3d13e5de5
AllienFrancis <jigsonjo@yahoo.com>
2022-10-29 02:54:42 -0700

    write to check for a digit 0 through 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,13 @@
 /**
  * _isdigit - checks for characters that are digits
  * @c: character
+ *
  * Return: 0 or 1
  */
 
 int _isdigit(init c)
 {
-	if ((c > 48) && (c < 60))
+	if ((c > 47) && (c < 58))
 	{
 		return (1);
 	}
