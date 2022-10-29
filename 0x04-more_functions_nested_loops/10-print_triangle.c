60b61269de8a69603bdd68ee7068ccbdd3eb740e
AllienFrancis <jigsonjo@yahoo.com>
2022-10-29 10:21:22 -0700

    Print triangle on the terminal

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using #
+ * @size: the size of the square
+ */
+void print_triangle(int size)
+{
+	int r, c, s;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (r = size; r > 0; r--)
+		{
+			c = size - r + 1;
+
+			for (s = r - 1; s > 0; s--)
+			{
+				_putchar(' ');
+			}
+
+			for (; c > 0; c--)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
