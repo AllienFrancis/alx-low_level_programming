0cb54a4635a38f898d9eadd7d1794d9f5e3fbe4f
AllienFrancis <jigsonjo@yahoo.com>
2022-10-31 06:20:57 -0700

    Fills memory with constant bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,15 @@ _memset - fills the first n bytes ofo the memory area pointed to by @s with the 
 *
 Return: A pointer to the filled memory area @s.
 */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
