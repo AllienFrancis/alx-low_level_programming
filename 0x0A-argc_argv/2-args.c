26d2bd89656153802d5d4564e4265a5ff96410f6
AllienFrancis <jigsonjo@yahoo.com>
2022-11-04 18:28:39 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - prints program name
+
+ * @argc: no of args
+
+ * @argv: pointer to arguments
+
+ *
+
+ * Return: 0 at exit
+
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	int p;
+
+	for (p = 0; p < argc; p++)
+
+	{
+
+		printf("%s\n", argv[p]);
+	}
+
+	return (0);
+
+}
