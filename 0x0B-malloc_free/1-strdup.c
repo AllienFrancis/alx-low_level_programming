231c17975c19fa2e5d9e8c3550cd45634813ce8d
AllienFrancis <jigsonjo@yahoo.com>
2022-11-07 19:10:19 -0800

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,60 +1,53 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
 
- * _strdup - returns a pointer to a newly allocated space in memory which
- * contains a copy of a string
- * @str: string
- * Return: pointer to allocated space
+ * _strdup - Return pointer to a new string that duplicates given string,
+ * allocate mem w/ malloc
+ * @str: String to duplicate
+ *
+ * Return: Pointer to new string, NULL if failed to make memory
  */
+
 char *_strdup(char *str)
 {
 
-	int i, str_len;
+	char *nstr;
 
-	char *str_copy;
+	unsigned int i, len;
 
 	if (str == NULL)
 
 		return (NULL);
 
-	str_len = _strlen(str);
+	i = len = 0;
 
-	str_copy = malloc(sizeof(char) * (str_len + 1));
+	while (str[len] != '\0')
 
-	if (str_copy == NULL)
+	{
+
+		len++;
+
+	}
+
+	len++;
+
+	nstr = malloc(len * sizeof(*str));
+
+	if (nstr == NULL)
 
 		return (NULL);
 
-	for (i = 0; str[i] != 0; i++)
+	while (i <= len)
 
-		str_copy[i] = str[i];
+	{
 
-	str_copy[i] = str[i];
-
-	return (str_copy);
-
-}
-
-
-/**
- * _strlen - computes the length of a string
- * @s: string
- * Return: length of s
- */
-int _strlen(char *s)
-
-{
-
-	int i;
-
-	i = 0;
-
-	while (s[i] != 0)
+		nstr[i] = str[i];
 
 		i++;
 
-	return (i);
+	}
+
+	return (nstr);
 
 }
