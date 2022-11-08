1fd28fe71e60e9b9ea5372366ea95ff3c9fa5cad
AllienFrancis <jigsonjo@yahoo.com>
2022-11-08 11:50:39 -0800

    Function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,76 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to new string
- */
 
+ * str_concat - a function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: a pointer to the new string
+ */
 char *str_concat(char *s1, char *s2)
 
 {
 
-	int i, j, s1_len, s2_len;
-
-	char *str;
-
-	s1_len = _strlen(s1), s2_len = _strlen(s2);
-
-	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
-
-	if (str == NULL)
-
-		return (NULL);
+	char *s3;
 
 	if (s1 == NULL)
 
 		s1 = "";
 
-	for (i = 0; s1[i] != 0; i++)
-
-		str[i] = s1[i];
-
 	if (s2 == NULL)
 
 		s2 = "";
 
+	s3 = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 
+	if (s3 == NULL)
 
-	j = i;
+		return (NULL);
 
-	for (i = 0; s2[i] != 0; i++)
+	strcpy(s3, s1);
+	strcat(s3, s2);
 
-		str[i + j] = s2[i];
+	return (s3);
 
-	str[i + j] = s2[i];
-
-	return (str);
-
-}
-
-/**
- * _strlen - computes the length of a string
- * @s: string
- * Return: length of s
- */
-
-int _strlen(char *s)
-{
-
-	int i;
-
-
-	if (s == NULL)
-
-		return (0);
-
-	i = 0;
-
-	while (s[i] != 0)
-
-		i++;
-
-	return (i);
 }
