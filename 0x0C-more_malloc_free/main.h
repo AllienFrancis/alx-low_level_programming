33d374a0b7e4fe976d64b5efde6255aae6956ed8
AllienFrancis <jigsonjo@yahoo.com>
2022-11-10 15:20:08 -0800

    Trust no one

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+int _putchar(char);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+
+#endif
